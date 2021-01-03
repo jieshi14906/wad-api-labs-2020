1b7a6cf7d6c04757d7b83f6dc445fb9700bda8e7
JIE SHI <20091665@mail.wit.ie>
2021-01-03 23:29:13 +0000

    Week 9 Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import {loadUsers} from './seedData'
+import usersRouter from './api/users';
 
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
