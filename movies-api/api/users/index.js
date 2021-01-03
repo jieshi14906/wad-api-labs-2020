30ab9691f8662aed437e04fffee5aa17a96b2ddd
JIE SHI <20091665@mail.wit.ie>
2021-01-03 23:43:49 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,9 +9,28 @@ router.get('/', (req, res) => {
 });
 
 // register
-router.post('/', (req, res ,next) => {
-    User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
-});
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
+  router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
 
 // Update a user
 router.put('/:id',  (req, res) => {
