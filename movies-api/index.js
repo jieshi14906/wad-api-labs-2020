5a8c4638a932f012219e1fb420fdcbc0ca281668
JIE SHI <20091665@mail.wit.ie>
2021-01-03 23:15:21 +0000

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,7 +1,10 @@
+import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import {loadUsers} from './seedData'
+
 
 dotenv.config();
 const errHandler = (err, req, res, next) => {
@@ -12,6 +15,9 @@ const errHandler = (err, req, res, next) => {
   }
   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
 };
+if (process.env.SEED_DB) {
+  loadUsers();
+}
 const app = express();
 
 const port = process.env.PORT;
